1f48acc178bbcd24ecd7793d279b6c9911ec60a9
FranciscaMora <fmoraparedes@gmail.com>
2018-12-06 10:04:14 -0300

    TestOK

--- index.js
+++ index.js
@@ -1,63 +1,66 @@
-window.onload=()=>{
+window.onload = () => {
     /*oculta pageOne */
-        document.getElementById("pageOne").style.display="none";
-        document.getElementById("instructions").style.display="none";
-        document.getElementById("recomendations").style.display="none";
-        
-    
-    }
-    
-    /*Oculta botón comenzar2*/
-    
-    
-    //**Muestra página instrucciones */
-    function functionInstrucciones(){
-        document.getElementById("pageZero").style.display="none";
-        document.getElementById("instructions").style.display="block";
-        document.getElementById("btnStart2").style.display="none"; 
-    }
-    /*oculta página inicial al clickear botón */
-    function functionStart(){
-        document.getElementById("pageZero").style.display="none";
-        document.getElementById("pageOne").style.display="block";
-        document.getElementById("instructions").style.display="none";
-    }
-    
-    function functionStart2(){
-        document.getElementById("instructions").style.display="none";
-        document.getElementById("pageOne").style.display="block";
-    
-    }
-    function functionInstrucciones(){
-        document.getElementById("pageZero").style.display="none";
-        document.getElementById("instructions").style.display="block";
-    
-    
-    }
-    
-    
-     function functionHome(){
-        
-        document.getElementById("pageOne").style.display="none";
-        document.getElementById("pageZero").style.display="block";
-        document.getElementById("codeBox").value="";
-        document.getElementById("decodeBox").value="";
-     }
-      function functionCode(){
-        let code=document.getElementById("codeBox").value;
-        let offset=parseInt(document.getElementById("difficulty").value);
-        let resultado= cipher.encode(code,offset);
-        document.getElementById("decodeBox").value =resultado;
-      }
-    
-      function functionDecode(){
-          let decode=document.getElementById("codeBox").value;
-          let offset=parseInt(document.getElementById("difficulty").value);
-          let resultado2= cipher.decode(decode,offset);
-          document.getElementById("decodeBox").value=resultado2;
-      }
+    document.getElementById("pageOne").style.display = "none";
+    document.getElementById("instructions").style.display = "none";
+    document.getElementById("recomendations").style.display = "none";
 
-      function functionClear(){
-          document.getElementById("codebox").value="";
-          document.getElementById("decodeBox").value="";
-      }
+
+}
+
+/*Oculta botón comenzar2*/
+
+
+//**Muestra página instrucciones */
+document.getElementById("btnInstructions").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("pageZero").style.display = "none";
+    document.getElementById("instructions").style.display = "block";
+    document.getElementById("btnStart2").style.display = "none";
+})
+/*oculta página inicial al clickear botón */
+document.getElementById("btnStart").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("pageZero").style.display = "none";
+    document.getElementById("pageOne").style.display = "block";
+    document.getElementById("instructions").style.display = "none";
+})
+
+document.getElementById("btnStart2").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("instructions").style.display = "none";
+    document.getElementById("pageOne").style.display = "block";
+
+})
+
+
+
+document.getElementById("btnHome").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("pageOne").style.display = "none";
+    document.getElementById("pageZero").style.display = "block";
+    document.getElementById("codeBox").value = "";
+    document.getElementById("decodeBox").value = "";
+})
+
+document.getElementById("btnCode").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let code = document.getElementById("codeBox").value;
+    let offset = parseInt(document.getElementById("difficulty").value);
+    let resultado = cipher.encode(code, offset);
+    document.getElementById("decodeBox").value = resultado;
+})
+
+document.getElementById("btnDecode").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let decode = document.getElementById("codeBox").value;
+    let offset = parseInt(document.getElementById("difficulty").value);
+    let resultado2 = cipher.decode(decode, offset);
+    document.getElementById("decodeBox").value = resultado2;
+})
+
+document.getElementById("clear").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    document.getElementById("difficulty").value = "";
+    document.getElementById("codeBox").value = "";
+    document.getElementById("decodeBox").value = "";
+})
