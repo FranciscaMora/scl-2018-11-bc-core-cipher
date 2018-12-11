b509947402d8df50c6eb2d5e2f11ecbb8e4144f1
FranciscaMora <fmoraparedes@gmail.com>
2018-12-11 19:09:53 -0300

    cipher_simbolos_especiales

--- src/cipher.js
+++ src/cipher.js
@@ -2,13 +2,25 @@ window.cipher = {
     encode: (code, offset) => {
         let result = "";
         for (let i = 0; i < code.length; i++) {
-            let newCode = code.toUpperCase();
-            let ascii = newCode.charCodeAt(i);
+            // let newCode = code
+            let ascii = code.charCodeAt(i);
             if (ascii === 32) {
                 result += " "
                 continue;
-            }
-            let formula = ((ascii - 65 + parseInt(offset)) % 26) + 65;
+            } else if (code[i] === "ñ") {
+                result += "ñ"
+                continue;
+            } else if (code[i] === "Ñ") {
+                result += "Ñ"
+                continue;
+            } else if (code[i] === "¡") {
+                result += "¡"
+                continue;
+            } else if (code[i] === "¿") {
+                result += "¿"
+                continue;
+            } else if ((code >= 32) && (code <= 126));
+            let formula = ((ascii - 32 + parseInt(offset)) % 94) + 32;
             let nuevoAscii = String.fromCharCode(formula);
             result += nuevoAscii;
 
@@ -22,15 +34,28 @@ window.cipher = {
     decode: (decode, offset) => {
         let resultado2 = "";
         for (let i = 0; i < decode.length; i++) {
-            let newCodeD = decode.toUpperCase();
-            let asciiD = newCodeD.charCodeAt(i);
+            // let newCodeD = decode
+            let asciiD = decode.charCodeAt(i);
             if (asciiD === 32) {
                 resultado2 += " "
                 continue;
+            } else if (decode[i] === "ñ") {
+                resultado2 += "ñ"
+                continue;
             }
-            let formula = ((asciiD - 90 - parseInt(offset)) % 26) + 90;
+            else if (decode[i] === "Ñ") {
+                resultado2 += "Ñ"
+                continue;
+            } else if (decode[i] === "¡") {
+                resultado2 += "¡"
+                continue;
+            } else if (decode[i] === "¿") {
+                resultado2 += "¿"
+                continue;
+            } else if ((decode <= 126) && (decode >= 32));
+            let formula = ((asciiD - 126 - parseInt(offset)) % 94) + 126;
             let nuevoAsciiD = String.fromCharCode(formula);
-            resultado2 += nuevoAsciiD;
+            resultado2 += nuevoAsciiD
 
         }
         return resultado2;
